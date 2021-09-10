14b70cf624ffbc1ba81213109f06fd16ac8c3fed
Dragoazer <flomat50@gmail.com>
2021-09-10 11:45:17 +0200

    début du projet

---
+++ pages/first-post.js
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export async function getSortedPostsData() {
+    var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
+}
+
+
+export default function FirstPost() {
+
+  return (
+   <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
